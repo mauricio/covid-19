ac2765557a7306611b91d48fe4a5ec2ff0f70a7f
Maurício Linhares <mlinhares@digitalocean.com>
2020-03-15 02:49:07 -0400

    I18n, creating messages file and adding Brazilian Portguese translations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React, { useState, useEffect, useRef, useMemo } from "react"
+import React, {useEffect, useMemo, useRef, useState} from "react"
 import serializeForm from "form-serialize"
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useNavigate,
-  useLocation,
-  Link
-} from "react-router-dom"
+import {BrowserRouter, Link, Route, Routes, useLocation, useNavigate} from "react-router-dom"
+import {FormattedMessage} from "react-intl";
 
 function App() {
   let [persons, setPersons] = useState(4)
@@ -26,7 +20,7 @@ function App() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    let values = serializeForm(event.target, { hash: true }).ages.filter(
+    let values = serializeForm(event.target, {hash: true}).ages.filter(
       v => v !== "UNSET"
     )
     navigate(`infected/?ages=${values.map(v => v)}`)
@@ -35,32 +29,31 @@ function App() {
   return (
     <div id="App">
       <div className="prelude">
-        <h1>Social Distancing</h1>
+        <h1><FormattedMessage id="app.title"/></h1>
         <p>
-          Many people close to me are skeptical that covid-19 is a big deal.
-          I've made this illustration to show how your action will affect you,
-          your household, and the community around you. All equations are based
-          on the latest infection growth and fatality rates of the virus as of
-          March 14, 2020.
+          <FormattedMessage id="app.first_paragraph"/>
         </p>
       </div>
-      <hr />
+      <hr/>
       <form id="HouseHoldForm" ref={formRef} onSubmit={handleSubmit}>
-        {Array.from({ length: persons }).map((_, index, arr) => (
+        {Array.from({length: persons}).map((_, index, arr) => (
           <label
             key={index}
             ref={arr.length - 1 === index ? focusRef : undefined}
           >
             <span>
-              {index === 0 ? "Your age" : `Household Member ${index}`}:
+              {index === 0 ? <FormattedMessage id="app.your_age"/> :
+                <FormattedMessage id="app.household_member" values={{index: index}}/>}:
             </span>{" "}
-            <AgeSelect defaultValue={index < 2 ? 40 : undefined} />
+            <AgeSelect defaultValue={index < 2 ? 40 : undefined}/>
           </label>
         ))}
         <button type="button" onClick={() => setPersons(persons + 1)}>
-          Add another
+          <FormattedMessage id="app.add_another"/>
         </button>
-        <button type="submit">Next</button>
+        <button type="submit">
+          <FormattedMessage id="app.next"/>
+        </button>
       </form>
     </div>
   )
@@ -70,7 +63,7 @@ function AgeSelect(props) {
   return (
     <select name="ages" {...props}>
       <option value="UNSET">- set an age</option>
-      {Array.from({ length: 100 }).map((_, index) => (
+      {Array.from({length: 100}).map((_, index) => (
         <option key={index}>{index}</option>
       ))}
     </select>
@@ -90,66 +83,77 @@ function Infection() {
   return (
     <div id="App">
       <div className="prelude">
-        <h1>You're Infected</h1>
+        <h1>
+          <FormattedMessage id="app.youre_infected"/>
+        </h1>
         <p>
-          Let's roll the dice and see if it kills any of your family. It
-          probably won't.
+          <FormattedMessage id="app.lets_roll"/>
         </p>
       </div>
       <div id="DiceRolls" className="center">
         {ages.map((age, index) => (
-          <DiceRoll key={index} age={age} />
+          <DiceRoll key={index} age={age}/>
         ))}
       </div>
       <p>
-        As expected, you probably didn't die. But it's not about you. Let's look
-        at how many people your family is going to kill by not practicing social
-        distancing.
+        <FormattedMessage id="app.did_not_die"/>
       </p>
       <Link className="big-link" to={`/killers${location.search}`}>
-        Your Kill Count ▸
+        <FormattedMessage id="app.your_kill_count"/>
       </Link>
 
-      <hr />
-      <h2>More information</h2>
+      <hr/>
+      <h2>
+        <FormattedMessage id="app.more_information"/>
+      </h2>
       <p>
-        Unless you're over 60, or are immuno-comprimised{" "}
-        <i>(lots of your friends and family are!)</i> you're going to have to
-        click the button a lot before you die.
-      </p>
-      <p>So this is just like the flu, right?</p>
-      <p>
-        Not quite. People have been quoting how many deaths per year there are
-        for the flu (
-        <a href="https://www.cdc.gov/flu/about/burden/index.html#:~:text=">
-          12,000 to 61,000
-        </a>
-        ) to the deaths so far with coronavirus (
-        <a href="https://www.cnn.com/interactive/2020/health/coronavirus-maps-and-cases/">
-          ~50
-        </a>
-        ) in the US.
+        <FormattedMessage
+          id="app.more_information_content"
+          values={{
+            i: (...chunks) => <i>{chunks}</i>,
+          }}
+        />
       </p>
       <p>
-        To get the full story you usually have to look at more than static
-        numbers. In this case, we need to look at:
+        <FormattedMessage id="app.just_like_flu"/>
+      </p>
+      <p>
+        <FormattedMessage
+          id="app.just_like_flu_content"
+          values={{
+            cdcburden: (...chunks) => <a class="external_link" target="_blank"
+                                         href="https://www.cdc.gov/flu/about/burden/index.html#:~:text=">{chunks}</a>,
+            cnnmaps: (...chunks) => <a class="external_link" target="_blank"
+                                       href="https://www.cnn.com/interactive/2020/health/coronavirus-maps-and-cases/">{chunks}</a>,
+          }}
+        />
+      </p>
+      <p>
+        <FormattedMessage id="app.full_story"/>
       </p>
       <ul>
-        <li>Fatality Rate</li>
-        <li>Infection Growth Rate</li>
+        <li>
+          <FormattedMessage id="app.fatality_rate"/>
+        </li>
+        <li>
+          <FormattedMessage id="app.infection_growth_rate"/>
+        </li>
       </ul>
       <p>
-        The flu has a general fatality rate of 0.1%
-        <br />
-        COVID-19's fatality rate right now is 3.4%
+        <FormattedMessage
+          id="app.fatality_rate_detail"
+        />
       </p>
       <p>
-        <b>
-          <a href="https://www.sciencealert.com/covid-19-s-death-rate-is-higher-than-thought-but-it-should-drop">
-            That's 34x
-          </a>
-        </b>
-        . The red bar here is 34 times bigger.
+        <FormattedMessage
+          id="app.fatality_rate_numbers"
+          values={{
+            b: (...chunks) => <b>{chunks}</b>,
+            p: (...chunks) => <p>{chunks}</p>,
+            deathrate: (...chunks) => <a class="external_link" target="_blank"
+                                         href="https://www.sciencealert.com/covid-19-s-death-rate-is-higher-than-thought-but-it-should-drop">{chunks}</a>,
+          }}
+        />
       </p>
 
       <div className="bars">
@@ -160,20 +164,15 @@ function Infection() {
           <span className="padding-adjust">Influenza</span>
         </div>
       </div>
-      <p>
-        It's easy to tell this virus is worse even without all the data,{" "}
-        <b>the flu doesn't completely overwhelm the health care system</b> in
-        Italy each year, but{" "}
-        <a href="https://www.theatlantic.com/ideas/archive/2020/03/who-gets-hospital-bed/607807/">
-          that's exactly what coronavirus has done
-        </a>
-        .
-      </p>
-      <p>But still, only ~50 deaths in the US right? What's the big deal?</p>
-      <p>
-        The big deal is mixing a fatality rate that's 34x of the flu with
-        exponential growth.
-      </p>
+      <FormattedMessage
+        id="app.fatality_rate_growth"
+        values={{
+          b: (...chunks) => <b>{chunks}</b>,
+          p: (...chunks) => <p>{chunks}</p>,
+          overwhelm: (...chunks) => <a class="external_link" target="_blank"
+                                       href="https://www.theatlantic.com/ideas/archive/2020/03/who-gets-hospital-bed/607807/">{chunks}</a>,
+        }}
+      />
     </div>
   )
 }
@@ -191,7 +190,7 @@ let rates = [
   [79, 0.148]
 ]
 
-function DiceRoll({ age }) {
+function DiceRoll({age}) {
   let [state, setState] = useState("alive") // alive, dead, rolling
   let [rolls, setRolls] = useState(0)
 
@@ -232,29 +231,34 @@ function DiceRoll({ age }) {
           {state === "dead"
             ? "💀"
             : state === "alive"
-            ? "😅"
-            : state === "rolling"
-            ? "🤮"
-            : null}
+              ? "😅"
+              : state === "rolling"
+                ? "🤮"
+                : null}
         </span>{" "}
         <span>
-          <b>{age} year old</b>
-          <br />
-          Fatality Rate: {(rate * 100).toFixed(1)}%
+          <b>
+            <FormattedMessage id="dice_roll.age_year_old" values={{age: age}}/>
+          </b>
+          <br/>
+          <FormattedMessage id="dice_roll.fatality_rate" values={{rate: (rate * 100).toFixed(1)}}/>
         </span>
       </div>
       <div>
         <button disabled={state === "dead"} onClick={rollDice}>
-          Roll the dice
-        </button>{" "}
-        <span>Rolls: {rolls}</span>
+          <FormattedMessage id="dice_roll.roll"/>
+        </button>
+        {" "}
+        <span>
+          <FormattedMessage id="dice_roll.rolls" values={{rolls: rolls}}/>
+        </span>
       </div>
     </div>
   )
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-function KillCount({ ages }) {
+function KillCount({ages}) {
   let [infected, setInfected] = useState(1)
   let [weeks, setWeeks] = useState(1)
   let rate = 0.034
@@ -270,15 +274,23 @@ function KillCount({ ages }) {
   return (
     <div id="KillCount">
       <div aria-hidden="true">
-        {Array.from({ length: killed }).map((_, index) => (
+        {Array.from({length: killed}).map((_, index) => (
           // eslint-disable-next-line
           <span key={index}>💀</span>
         ))}
       </div>
-      <p>Week: {weeks}</p>
-      <p>People You Infected: {infected}</p>
-      <p>People You Killed: {killed}</p>
-      <button onClick={nextWeek}>Live another week</button>
+      <p>
+        <FormattedMessage id="kill_count.week" values={{weeks: weeks}}/>
+      </p>
+      <p>
+        <FormattedMessage id="kill_count.infected" values={{infected: infected}}/>
+      </p>
+      <p>
+        <FormattedMessage id="kill_count.killed" values={{killed: killed}}/>
+      </p>
+      <button onClick={nextWeek}>
+        <FormattedMessage id="kill_count.live_another"/>
+      </button>
     </div>
   )
 }
@@ -295,66 +307,63 @@ function Killers() {
   return (
     <div id="App">
       <div className="prelude">
-        <h1>Your Kill Count</h1>
-        <p>
-          Social distancing is about how many people you want to kill (I hope
-          that's zero). Right now, a COVID-19 infected person infects two more.
-        </p>
-        <p>
-          So you infect two people, and next week they infect two people each,
-          and then they infect two more, etc. etc.
-        </p>
-        <p>Go ahead and click the button</p>
+        <FormattedMessage
+          id="killers.your_kill_count"
+          values={{
+            h1: (...chunks) => <h1>{chunks}</h1>,
+            p: (...chunks) => <p>{chunks}</p>,
+          }}
+        />
       </div>
-      <KillCount ages={ages} />
+      <KillCount ages={ages}/>
       <p>
-        So please, stay home. And while you're there{" "}
-        <a href="https://medium.com/@joschabach/flattening-the-curve-is-a-deadly-delusion-eea324fe9727">
-          I think this article is worth your time.
-        </a>
-        . Containment seems to be the best action right now given the numbers.
+        <FormattedMessage
+          id="killers.stay_home"
+          values={{
+            stayhome: (...chunks) => <a class="external_link" target="_blank"
+                                        href="https://medium.com/@joschabach/flattening-the-curve-is-a-deadly-delusion-eea324fe9727">{chunks}</a>,
+          }}
+        />
       </p>
-      <hr />
-      <h2>More information</h2>
+      <hr/>
+      <h2>
+        <FormattedMessage id="app.more_information"/>
+      </h2>
       <p>
-        <Link to={`/infected${location.search}`}>
-          On the previous page we looked at the fatality rate
-        </Link>{" "}
-        of COVID-19 and saw that statistically, you and your family will
-        probably be fine, but social distancing isn't about you.
+        <FormattedMessage
+          id="killers.previous_page_content"
+          values={{
+            link: (...chunks) => <Link to={`/infected${location.search}`}>
+              {chunks}
+            </Link>,
+          }}
+        />
       </p>
-      <p>
-        What really sucks about this virus vs. the flu is that you can carry it
-        around with you for weeks infecting people before you even know you have
-        it. There is no way to know until it's too late. That's why containment
-        is so important.
-      </p>
-      <p>
-        You just saw that logarithmic growth is a powerful thing. The
-        coronavirus is following a nearly perfect logarithmic curve in the
-        US--even with our low levels of testing. While network marketers hope to
-        use it in their favor to gain financial independence, a virus doesn't
-        have to be convinced to keep the downline going.
-      </p>
+      <FormattedMessage
+        id="killers.what_sucks"
+        values={{
+          p: (...chunks) => <p>{chunks}</p>,
+        }}
+      />
       <a
-        style={{ display: "block", border: "solid 1px" }}
+        style={{display: "block", border: "solid 1px"}}
         href="https://www.worldometers.info/coronavirus/country/us/"
       >
         <img
-          style={{ width: "100%" }}
+          style={{width: "100%"}}
           alt="graph showing a nearly perfect algorithmic growth rate"
           src="/graph.png"
         />
       </a>
       <p>
-        The <i>Attack Rate</i> of COVID-19 is estimated by the World Health
-        Organization to be{" "}
-        <a href="https://www.worldometers.info/coronavirus/#repro">
-          between 1.4 and 2.5
-        </a>
-        . That means if you get it, you're going to infect 2 other people (other
-        studies have it as high as 4!). By comparison, the flu is 1.3 and
-        anything less than 1 will just die off.
+        <FormattedMessage
+          id="killers.attack_rate"
+          values={{
+            i: (...chunks) => <i>{chunks}</i>,
+            a: (...chunks) => <a class="external_link" target="_blank"
+                                 href="https://www.worldometers.info/coronavirus/#repro">{chunks}</a>,
+          }}
+        />
       </p>
     </div>
   )
@@ -380,15 +389,15 @@ function AppRoot() {
 
   return (
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/infected" element={<Infection />} />
-      <Route path="/killers" element={<Killers />} />
+      <Route path="/" element={<App/>}/>
+      <Route path="/infected" element={<Infection/>}/>
+      <Route path="/killers" element={<Killers/>}/>
     </Routes>
   )
 }
 
 export default () => (
   <BrowserRouter>
-    <AppRoot />
+    <AppRoot/>
   </BrowserRouter>
 )
---
+++ src/Messages.jsx
@@ -0,0 +1,144 @@
+export const Messages = {
+  "en": {
+    "app.title": "Social Distancing",
+    "app.first_paragraph": "Many people close to me are skeptical that covid-19 is a big deal. I've made this illustration to show how your action will affect you, your household, and the community around you. All equations are based on the latest infection growth and fatality rates of the virus as of March 14, 2020.",
+    "app.your_age": "Your Age",
+    "app.household_member": "Household Member {index}",
+    "app.next": "Next",
+    "app.add_another": "Add another",
+    "app.youre_infected": "You're infected",
+    "app.lets_roll": "Let's roll the dice and see if it kills any of your family. It probably won't.",
+    "app.did_not_die": "As expected, you probably didn't die. But it's not about you. Let's look at how many people your family is going to kill by not practicing social distancing.",
+    "app.your_kill_count": "Your Kill Count ▸",
+    "app.more_information": "More information",
+    "app.more_information_content": "Unless you're over 60, or are immunocompromised <i>(lots of your friends and family are!)</i> you're going to have to click the button a lot before you die.",
+    "app.just_like_flu": "So this is just like the flu, right?",
+    "app.just_like_flu_content": 'Not quite. People have been quoting how many deaths per year there are for the flu (<cdcburden>12,000 to 61,000</cdcburden>) to the deaths so far with coronavirus (<cnnmaps>~50</cnnmaps>) in the US.',
+    "app.full_story": "To get the full story you usually have to look at more than static numbers. In this case, we need to look at:",
+    "app.fatality_rate": "Fatality Rate",
+    "app.infection_growth_rate": "Infection Growth Rate",
+    "app.fatality_rate_detail": "The flu has a general fatality rate of 0.1%, COVID-19's fatality rate right now is 3.4%",
+    "app.fatality_rate_numbers": `<b>
+          <deathrate>
+            That's 34x
+          </deathrate>
+        </b>
+        . The red bar here is 34 times bigger.`,
+    "app.fatality_rate_growth": `<p>It's easy to tell this virus is worse even without all the data, <b>the flu doesn't completely overwhelm the health care system</b> in Italy each year, but <overwhelm>
+          that's exactly what coronavirus has done
+        </overwhelm>.
+      </p>
+      <p>But still, only ~50 deaths in the US right? What's the big deal?</p>
+      <p>
+        The big deal is mixing a fatality rate that's 34x of the flu with exponential growth.
+      </p>`,
+    "dice_roll.age_year_old": "{age} year old",
+    "dice_roll.fatality_rate": "Fatality Rate: {rate}%",
+    "dice_roll.roll": "Roll the dice",
+    "dice_roll.rolls": "Rolls: {rolls}",
+    "kill_count.week": "Week: {weeks}",
+    "kill_count.infected": "People You Infected: {infected}",
+    "kill_count.killed": "People You Killed: {killed}",
+    "kill_count.live_another": "Live another week",
+    "killers.your_kill_count": `<h1>Your Kill Count</h1>
+        <p>
+          Social distancing is about how many people you want to kill (I hope
+          that's zero). Right now, a COVID-19 infected person infects two more.
+        </p>
+        <p>
+          So you infect two people, and next week they infect two people each,
+          and then they infect two more, etc. etc.
+        </p>
+        <p>Go ahead and click the button</p>`,
+    "killers.stay_home": `So please, stay home. And while you're there <stayhome>
+          I think this article is worth your time
+        </stayhome>. Containment seems to be the best action right now given the numbers.`,
+    "killers.previous_page_content": "<link>On the previous page we looked at the fatality rate </link> of COVID-19 and saw that statistically, you and your family will probably be fine, but social distancing isn't about you.",
+    "killers.what_sucks": `<p>
+        What really sucks about this virus vs. the flu is that you can carry it
+        around with you for weeks infecting people before you even know you have
+        it. There is no way to know until it's too late. That's why containment
+        is so important.
+      </p>
+      <p>
+        You just saw that logarithmic growth is a powerful thing. The
+        coronavirus is following a nearly perfect logarithmic curve in the
+        US--even with our low levels of testing. While network marketers hope to
+        use it in their favor to gain financial independence, a virus doesn't
+        have to be convinced to keep the downline going.
+      </p>`,
+    "killers.attack_rate": `The <i>Attack Rate</i> of COVID-19 is estimated by the World Health
+        Organization to be <a>between 1.4 and 2.5</a>. That means if you get it, you're going to infect 2 other people (other
+        studies have it as high as 4!). By comparison, the flu is 1.3 and
+        anything less than 1 will just die off.`,
+  },
+  "pt": {
+    "app.title": "Distanciamento social",
+    "app.first_paragraph": "Muitas pessoas próximas a mim estão céticas sobre o tamanho do problema do COVID-19. Fiz esta página para mostrar como o seu comportamento afetará você, sua família e a comunidade ao seu redor. Todas as estatísticas são baseadas nas mais recentes taxas de crescimento de infecções e mortalidade do vírus em 14 de março de 2020.",
+    "app.your_age": "Sua idade",
+    "app.household_member": "Membro da família {index}",
+    "app.next": "Próximo",
+    "app.add_another": "Adicionar outro/a",
+    "app.youre_infected": "Você está infectado",
+    "app.lets_roll": "Role o dado e veja se alguém da sua família vai morrer. Provavelmente ninguém vai morrer.",
+    "app.did_not_die": "Como esperado, você provavelmente não morreu. Mas isso não é sobre você. Vamos ver quantas pessoas a sua família vai matar por não praticar distanciamento social.",
+    "app.your_kill_count": "Pessoas que você matou ▸",
+    "app.more_information": "Mais informações",
+    "app.more_information_content": "A não ser que você tenha mais de 60 anos, ou seja imunossuprimido <i>(muitos dos seus amigos ou familiares podem ser!)</i> você vai ter que apertar o botão várias vezes pra morrer.",
+    "app.just_like_flu": "Então isso é a mesma coisa que a gripe, certo?",
+    "app.just_like_flu_content": 'Não exatamente. As pessoas estão falando do número de mortos pela gripe (<cdcburden>de 12.000 a 61.000</cdcburden>) com a quantidade de mortos do coronavírus (<cnnmaps>~50</cnnmaps>) nos Estados Unidos.',
+    "app.full_story": "Pra entender de verdade precisamos olhar além dos números isolados. Nesse caso, precisamos olhar para:",
+    "app.fatality_rate": "Taxa de Mortalidade",
+    "app.infection_growth_rate": "Taxa de crescimento das infecções",
+    "app.fatality_rate_detail": "A gripe tem uma taxa de mortalidade de 0.1%, já o COVID-19 tem como sua taxa de mortalidade atual em 3.4%",
+    "app.fatality_rate_numbers": `<b>
+          <deathrate>
+            Isso é 34x a da gripe
+          </deathrate>
+        </b>
+        . A barra vermelha é 34 vezes maior.`,
+    "app.fatality_rate_growth": `<p>É fácil de mostrar que esse vírus é pior mesmo sem todos os dados, <b>a gripe não sobrecarrega completamente o sistema de saúde</b> na Itália todo ano, mas <overwhelm>
+          é exatamente isso que o coronavírus está fazendo
+        </overwhelm>.
+      </p>
+      <p>
+      Mas então, são apenas ~50 mortos nos Estados Unidos no momento, certo? Por que se preocupar tanto?
+      </p>
+      <p>
+        O grande problema é juntar a taxa de fatalidade 34x maior do que a gripe com crescimento exponencial.
+      </p>`,
+    "dice_roll.age_year_old": "{age} anos",
+    "dice_roll.fatality_rate": "Taxa de mortalidade: {rate}%",
+    "dice_roll.roll": "Role o dado",
+    "dice_roll.rolls": "Rolagens: {rolls}",
+    "kill_count.week": "Semanas: {weeks}",
+    "kill_count.infected": "Pessoas que você infectou: {infected}",
+    "kill_count.killed": "Pessoas que você matou: {killed}",
+    "kill_count.live_another": "Viva mais uma semana",
+    "killers.your_kill_count": `<h1>Pessoas que você matou</h1>
+        <p>
+          Praticar distanciamento social define quantas pessoas você quer infectar e até matar (eu espero que seja zero).
+          Atualmente, cada pessoa infectada pelo coronavírus infecta mais duas pessoas.
+        </p>
+        <p>
+          Então você infecta duas pessoas, na semana que vem eles infectam mais duas pessoas,
+          depois cada um infecta mais duas e assim por diante.
+        </p>
+        <p>Aperte o botão pra ver os resultados</p>`,
+    "killers.stay_home": `Então fique em casa. E já que você não vai fazer nada <stayhome>
+          esse artigo vale a leitura
+        </stayhome>. Contenção e isolamento atualmente são o melhor comportamento dados os números.`,
+    "killers.previous_page_content": "<link>Na página anterior nós olhamos pra taxa de mortalidade </link> do COVID-19 e vimos que, estatísticamente, você e sua família provavelmente não vão ter problemas, mas a questão não é você e a sua família.",
+    "killers.what_sucks": `<p>
+        O que é realmente ruim sobre esse vírus comparado com a gripe é que você pode carregá-lo
+         por aí com você por semanas infectando pessoas antes mesmo de você saber que está infectado. 
+         Não há como saber até que seja tarde demais. É por isso que a contenção é tão importante.
+      </p>
+      <p>
+      Você acabou de ver que o crescimento logarítmico é rápido. 
+      O coronavírus está seguindo uma curva logarítmica quase perfeita no
+      EUA - mesmo com os baixos níveis de teste.
+      </p>`,
+    "killers.attack_rate": `A Organização Mundial de Saúde estima que a <i>Taxa de Infecção</i> do COVID-19 está <a>entre 1.4 e 2.5</a>. isso significa que se você for infectado, você vai infectar duas outras pessoas (outros estudos definem ela como sendo até 4!). Comparando, a da gripe é de 1.3 e qualquer coisa com taxa menor do que 1 simplesmente desaparece naturalmente.`,
+  },
+};
